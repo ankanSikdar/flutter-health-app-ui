3cd428ae026b1a7bee2691cb9556ac856055083c
Ankan Sikdar <ankansikdar@gmail.com>
2021-06-27 00:41:46 +0530

    Added google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:health_ui/pages/goals_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.workSansTextTheme(),
       ),
       home: GoalsPage(),
     );
